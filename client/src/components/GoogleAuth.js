6f89288df38bf8522ca049eee815f666536c5812
jakethecoder95 <95jacob07@gmail.com>
2019-03-09 21:33:53 -0800

    Merging all projects into one

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "791088117093-km7aaqd3b7qstq33v0nnmco4og9tnu0j.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = isSignedIn => {
+    if (isSignedIn === true) {
+      this.props.signIn(
+        window.gapi.auth2
+          .getAuthInstance()
+          .currentUser.get()
+          .getId()
+      );
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
---
+++ client/src/components/streams/StreamDelete.js
@@ -0,0 +1,52 @@
+import React, { Component, Fragment } from "react";
+import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class StreamDelete extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+    return (
+      <Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </Fragment>
+    );
+  }
+
+  render() {
+    const streamName = this.props.stream
+      ? `with title: "${this.props.stream.title}"`
+      : "";
+    return (
+      <Modal
+        title="Delete Stream"
+        content={`Are you sure you want to Delete your stream ${streamName} `}
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/")}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
